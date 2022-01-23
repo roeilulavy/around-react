4308fdef195897553a42214d2e87c1215a5fa25f
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-01-23 11:34:33 +0200

    Use context in Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,49 @@
-function Card(props){
+import { useContext } from "react";
+import { CurrentUserContext } from "../context/CurrentUserContext";
+
+function Card({card, id, link, title, likes, onCardClick, onLikeClick, onDeleteClick}){
+
+  const currentUser = useContext(CurrentUserContext);
+  const isOwn = card.owner._id === currentUser._id;
+  const isLiked = card.likes.some(like => like._id === currentUser._id);
+
+  const cardLikeButtonClassName = (
+    `card__delete-button ${isLiked ? 'elements__like-button_active' : 'elements__like-button'}`);
+
+  const cardDeleteButtonClassName = (
+    `card__delete-button ${isOwn ? 'elements__delete-button' : 'elements__delete-button_hidden'}`);
   
   function handleClick(){
-    props.onCardClick(props.card);
+    onCardClick(card);
+  }
+
+  function handleLikeButton(){
+    onLikeClick(card);
+  }
+
+  function handleDeleteButton(){
+    onDeleteClick(card);
   }
 
   return(
         <article className="elements__element">
-          <img className="elements__image" src={props.link} alt={props.name} onClick={handleClick} />
+          <img className="elements__image" src={card.link} alt={card.name} onClick={handleClick} />
           <button
-            className="elements__delete-button"
+            className={cardDeleteButtonClassName}
+            // "elements__delete-button"
             type="button"
             name="delete-button"
+            onClick={handleDeleteButton}
           ></button>
           <div className="elements__holder">
-            <h2 className="elements__caption">{props.title}</h2>
+            <h2 className="elements__caption">{title}</h2>
             <div className="elements__like-wrapper">
-              <button type="button" className="elements__like-button"></button>
-              <p className="elements__like-counter">{props.likes}</p>
+              <button type="button"
+              className={cardLikeButtonClassName}
+              // "elements__like-button"
+              onClick={handleLikeButton}>
+              </button>
+              <p className="elements__like-counter">{likes}</p>
             </div>
           </div>
         </article>
--- src/components/Main.js
+++ src/components/Main.js
@@ -7,7 +7,7 @@ import { CurrentUserContext } from "../context/CurrentUserContext";
 
 function Main(props) {
 
-  const currentUserContext = useContext(CurrentUserContext);
+  const currentUser = useContext(CurrentUserContext);
 
   const [cards, setCards] = useState([])
 
@@ -34,14 +34,14 @@ function Main(props) {
 
       <section className="profile">
         <div className="profile__profile-holder">
-          <img className="profile__profile-image" src={currentUserContext.avatar} alt="User avatar" />
+          <img className="profile__profile-image" src={currentUser.avatar} alt="User avatar" />
           <button type="button" className="profile__edit-picture" onClick={props.onEditAvatarClick}></button>
         </div>
 
         <div className="profile__profile-info">
-          <h1 className="profile__title">{currentUserContext.name}</h1>
+          <h1 className="profile__title">{currentUser.name}</h1>
           <button type="button" className="profile__edit-button" onClick={props.onEditProfileClick}></button>
-          <p className="profile__description">{currentUserContext.about}</p>
+          <p className="profile__description">{currentUser.about}</p>
         </div>
         
         <button type="button" className="profile__add-button" onClick={props.onAddPlaceClick}></button>
