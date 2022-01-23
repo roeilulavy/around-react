8c3b20ad03d17db106da4cc36b13fe920bff3e55
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-01-23 11:04:57 +0200

    currentUser state

--- src/components/App.js
+++ src/components/App.js
@@ -4,70 +4,97 @@ import Footer from "./Footer";
 import PopupWithForm from "./PopupWithForm";
 import ImagePopup from "./ImagePopup";
 import "../index.css";
-import { useState } from 'react';
+import { api } from "../utils/api";
+import React, { useState } from "react";
+import { CurrentUserContext } from "../context/CurrentUserContext";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState({});
+  const [isEditAvatarPopupOpen, setStateEditAvatarPopup] = useState(false);
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
+  const [isAddPlacePopupOpen, setStateAddPlacePopup] = useState(false);
+  const [selectedCard, setSelectedCard] = useState({});
+  const [isImagePopupOpen, setStateImagePopup] = useState(false);
 
-  const [isEditAvatarPopupOpen, setStateEditAvatarPopup] = useState(false)
-  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false)
-  const [isAddPlacePopupOpen, setStateAddPlacePopup] = useState(false)
-  const [selectedCard, setSelectedCard] = useState({})
-  const [isImagePopupOpen, setStateImagePopup] = useState(false)
+  React.useEffect(() => {
+    try {
+      async function fetchData() {
+        const userInfo = api.getUserData();
+
+        if (userInfo) {
+          setCurrentUser(userInfo);
+          console.log("userInfo Success! :" + userInfo);
+        }
+      }
+      fetchData();
+    } catch (error) {
+      console.log("Error! ", error);
+      alert("Something went wrong..");
+    }
+  }, []);
 
   function closeAllPopups() {
-    setIsEditProfilePopupOpen(false)
-    setStateAddPlacePopup(false)
-    setStateEditAvatarPopup(false)
-    setStateImagePopup(false)
+    setIsEditProfilePopupOpen(false);
+    setStateAddPlacePopup(false);
+    setStateEditAvatarPopup(false);
+    setStateImagePopup(false);
   }
 
   function handleEditAvatarClick() {
-    setStateEditAvatarPopup(true)
+    setStateEditAvatarPopup(true);
   }
 
   function handleEditProfileClick() {
-    setIsEditProfilePopupOpen(true)
+    setIsEditProfilePopupOpen(true);
   }
 
   function handleAddPlaceClick() {
-    setStateAddPlacePopup(true)
-  } 
+    setStateAddPlacePopup(true);
+  }
 
   function handleCardClick(props) {
-    setSelectedCard(props)
-    setStateImagePopup(true)
+    setSelectedCard(props);
+    setStateImagePopup(true);
   }
 
   return (
+    <CurrentUserContext.Provider value={currentUser}>
       <div className="page">
         <div className="page__wrapper">
           <Header />
           <Main
-          onEditAvatarClick={handleEditAvatarClick}
-          onEditProfileClick={handleEditProfileClick}
-          onAddPlaceClick={handleAddPlaceClick}
-          onCardClick={handleCardClick}
+            onEditAvatarClick={handleEditAvatarClick}
+            onEditProfileClick={handleEditProfileClick}
+            onAddPlaceClick={handleAddPlaceClick}
+            onCardClick={handleCardClick}
           />
 
-          <ImagePopup selectedCard={selectedCard} isOpen={isImagePopupOpen} onClose={closeAllPopups} />
+          <ImagePopup
+            selectedCard={selectedCard}
+            isOpen={isImagePopupOpen}
+            onClose={closeAllPopups}
+          />
 
           <PopupWithForm
             isOpen={isEditAvatarPopupOpen}
             name="edit-profile-picture"
             title="Change profile picture"
             onClose={closeAllPopups}
-            buttonText="Save">
-
+            buttonText="Save"
+          >
             <input
               id="input_type_url_photo"
               type="url"
               name="avatar"
               className="popup__input popup__input_type_avatar-link"
               placeholder="Image link"
-              required/>
+              required
+            />
 
-            <span id="input_type_url_photo-error" className="popup__error"></span>
-
+            <span
+              id="input_type_url_photo-error"
+              className="popup__error"
+            ></span>
           </PopupWithForm>
 
           <PopupWithForm
@@ -75,8 +102,8 @@ function App() {
             name="edit-profile"
             title="Edit Profile"
             onClose={closeAllPopups}
-            buttonText="Save">
-
+            buttonText="Save"
+          >
             <input
               id="input_type_name"
               type="text"
@@ -85,7 +112,8 @@ function App() {
               placeholder="Name"
               minLength="2"
               maxLength="40"
-              required/>
+              required
+            />
 
             <span id="input_type_name-error" className="popup__error"></span>
 
@@ -97,19 +125,22 @@ function App() {
               placeholder="About me"
               minLength="2"
               maxLength="200"
-              required/>
+              required
+            />
 
-            <span id="input_type_description-error" className="popup__error"></span>
-
+            <span
+              id="input_type_description-error"
+              className="popup__error"
+            ></span>
           </PopupWithForm>
 
-          < PopupWithForm
+          <PopupWithForm
             isOpen={isAddPlacePopupOpen}
             name="add-card"
             title="New Place"
             onClose={closeAllPopups}
-            buttonText="Create">
-
+            buttonText="Create"
+          >
             <input
               id="input_type_title"
               type="text"
@@ -119,7 +150,8 @@ function App() {
               minLength="1"
               maxLength="30"
               autoComplete="off"
-              required/>
+              required
+            />
 
             <span id="input_type_title-error" className="popup__error"></span>
 
@@ -129,22 +161,23 @@ function App() {
               name="link"
               className="popup__input popup__input_type_card-link"
               placeholder="Image link"
-              required/>
+              required
+            />
 
             <span id="input_type_url-error" className="popup__error"></span>
-
           </PopupWithForm>
 
           <PopupWithForm
             name="delete-card"
             title="Are you sure?"
             onClose={closeAllPopups}
-            buttonText="Yes">
-          </PopupWithForm>
+            buttonText="Yes"
+          ></PopupWithForm>
 
           <Footer />
         </div>
       </div>
+    </CurrentUserContext.Provider>
   );
 }
 
