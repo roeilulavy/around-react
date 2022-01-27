da6a8f1f5e287085dd8a2e09ca4e45ab01cbdfd2
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-01-27 14:02:04 +0200

    PopupWithForm.js fix - No changes

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,6 +1,6 @@
 function PopupWithForm(props) {
   return (
-    <div className={`popup popup_type_${props.name} ${props.isOpen ? 'popup_is-open' : ""}`}>
+    <div className={`popup popup_type_${props.name} ${props.isOpen && 'popup_is-open'}`}>
         <div className="popup__content">
           <button type="button" className="popup__close" onClick={props.onClose}></button>
           <h3 className="popup__title">{props.title}</h3>
